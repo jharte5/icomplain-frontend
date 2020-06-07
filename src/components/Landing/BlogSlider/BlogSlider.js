050cd5a5fdb198de01b760310868ffebae80c6e5
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-06-07 15:44:38 -0400

    init

---
+++ src/components/Landing/BlogSlider/BlogSlider.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './BlogSlider.css';
+
+export default class BlogSlider extends Component {
+  render() {
+    return (
+      <div className="blogslider">
+        <h1>BlogSlider</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import BlogSlider from './BlogSlider/BlogSlider';
+import Login from './Auth/Login/Login';
+// import Signup from './Auth/Signup/Signup';
+import './Landing.css';
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <BlogSlider />
+        <Login />
+      </div>
+    );
+  }
+}
