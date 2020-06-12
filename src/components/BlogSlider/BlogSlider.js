93df6ec19ff9d91c7f39906d3d9ea92d1fa79ccc
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-06-12 09:29:13 -0400

    working on add blog still

--- src/components/BlogSlider/BlogSlider.js
+++ src/components/BlogSlider/BlogSlider.js
@@ -6,31 +6,50 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 export default class extends Component {
   render() {
     return (
-      <div className="blogslider">
-        <CarouselProvider
-          naturalSlideWidth={350}
-          naturalSlideHeight={350}
-          visibleSlides={1}
-          totalSlides={10}
-          orientation="vertical"
-          interval={5000}
-          isPlaying={true}
-          step={1}
-        >
-          <Slider>
-            <Slide index={0}>Slide 1</Slide>
-            <Slide index={1}>Slide 2</Slide>
-            <Slide index={2}>Slide 3</Slide>
-            <Slide index={3}>Slide 4</Slide>
-            <Slide index={4}>Slide 5</Slide>
-            <Slide index={5}>Slide 6</Slide>
-            <Slide index={6}>Slide 7</Slide>
-            <Slide index={7}>Slide 8</Slide>
-            <Slide index={8}>Slide 9</Slide>
-            <Slide index={9}>Slide 10</Slide>
-          </Slider>
-        </CarouselProvider>
-      </div>
+      <CarouselProvider
+        className="carousel"
+        naturalSlideWidth={1}
+        naturalSlideHeight={1}
+        visibleSlides={1}
+        totalSlides={10}
+        orientation="vertical"
+        interval={5000}
+        isPlaying={true}
+        step={1}
+      >
+        <Slider className="slider">
+          <Slide className="slide" index={0}>
+            Slide 1
+          </Slide>
+          <Slide className="slide" index={1}>
+            Slide 2
+          </Slide>
+          <Slide className="slide" index={2}>
+            Slide 3
+          </Slide>
+          <Slide className="slide" index={3}>
+            Slide 4
+          </Slide>
+          <Slide className="slide" index={4}>
+            Slide 5
+          </Slide>
+          <Slide className="slide" index={5}>
+            Slide 6
+          </Slide>
+          <Slide className="slide" index={6}>
+            Slide 7
+          </Slide>
+          <Slide className="slide" index={7}>
+            Slide 8
+          </Slide>
+          <Slide className="slide" index={8}>
+            Slide 9
+          </Slide>
+          <Slide className="slide" index={9}>
+            Slide 10
+          </Slide>
+        </Slider>
+      </CarouselProvider>
     );
   }
 }
